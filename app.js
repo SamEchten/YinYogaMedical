33f4e46be972993edb4b8b6fee75aa834348ab1e
SamEchten <SamEchten@users.noreply.github.com>
2022-04-28 14:12:29 +0200

    Added modules / start app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const port = 3030;
+
+app.listen(port, () => {
+    console.log("app is running");
+    
+});
