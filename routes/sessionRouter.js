fce2d98d63919492f8ab0bfa0c37d574adbe79ef
SamEchten <SamEchten@users.noreply.github.com>
2022-05-18 10:22:31 +0200

    Session controller comments

--- routes/sessionRouter.js
+++ routes/sessionRouter.js
@@ -2,12 +2,19 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/sessionController");
 
+//Get all sessions ->
 router.get("/", controller.get);
+//Get single session ->
 router.get("/:id", controller.get);
+//Add new session ->
 router.post("/", controller.add);
+//Signs a user in for a session ->
 router.post("/signup/:id", controller.signup);
+//Signs a user out for a session ->
 router.post("/signout/:id", controller.signout);
+//Updates a session ->
 router.put("/:id", controller.update);
+//Deletes a session ->
 router.delete("/:id", controller.delete);
 
 module.exports = router;
