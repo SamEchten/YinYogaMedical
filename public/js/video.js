34cf2621ecfddc86ab4be3aca4781f3c5f63eab1
minxae <39745649+minxae@users.noreply.github.com>
2022-06-01 15:33:01 +0200

    updated video

--- public/js/video.js
+++ public/js/video.js
@@ -1,3 +1,27 @@
 $(async function () {
     console.log("video page");
   });
+
+  
+$(".addVideo").on("click", function() {
+    addVideo();
+});
+
+function addVideo () {
+    if(roleCheck()) {
+        Swal.fire({
+            title: 'Video toevoegen',
+            html: `<form>
+                <div class="form-group">
+                <label for="exampleFormControlFile1">Example file input</label>
+                <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                </div>
+            </form>`,
+            showCancelButton: true,
+            text: "",
+            confirmButtonColor: '#D5CA9B',
+            confirmButtonText: 'Toevoegen',
+            cancelButtonText: 'Terug'
+          });
+    }
+}
