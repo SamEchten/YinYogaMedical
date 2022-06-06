1f5b4ec693d33808e47a5ab1a47d74e6477c8b06
Bram Jansen <39745649+minxae@users.noreply.github.com>
2022-06-06 23:35:10 +0200

    videos

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -9,26 +9,26 @@ module.exports.streamFile = async (req, res) => {
 
     // Ensure there is a range given for the video
     const range = req.headers.range;
-    if (!range) {
+    console.log(range)
+    if (range) {
         res.status(400).send("Requires Range header");
+        return;
     }
 
     // get video stats (about 61MB)
-    const videoPath = "./media/videos/" + id + ".mp4";
-    const videoSize = fs.statSync("./media/videos/" + id + ".mp4").size;
+    const videoPath = path.join(__dirname, "../videos/" + id);
+    const videoSize = fs.statSync(__dirname, "../videos/" + id).size;
 
+    console.log(videoPath, "asd")
     // Parse Range
     // Example: "bytes=32324-"
     const CHUNK_SIZE = 10 ** 6; // 1MB
-    const start = Number(range.replace(/\D/g, ""));
-    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+
 
     // Create headers
-    const contentLength = end - start + 1;
+
     const headers = {
-        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
         "Accept-Ranges": "bytes",
-        "Content-Length": contentLength,
         "Content-Type": "video/mp4",
     };
 
@@ -36,7 +36,7 @@ module.exports.streamFile = async (req, res) => {
     res.writeHead(206, headers);
 
     // create video read stream for this particular chunk
-    const videoStream = fs.createReadStream(videoPath, { start, end });
+    const videoStream = fs.createReadStream(videoPath);
 
     // Stream the video chunk to the client
     videoStream.pipe(res);
