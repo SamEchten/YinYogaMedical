bf6af2c08100c60a8dc9c8cc605e00fce4d12e5f
Tom Olijve <55793184+goemel@users.noreply.github.com>
2022-06-07 15:50:09 +0200

    update profile

--- public/js/profile.js
+++ public/js/profile.js
@@ -10,10 +10,9 @@ $(".setting").on("click", async function () {
         $(".settingsContent").load("profile/myProfile", function (){
             $("#accountName").html(``+ json.fullName +`<i class="bi bi-pencil" onclick="changeInfo('name')"></i>`);
             $("#accountEmail").html(``+ json.email +`<i class="bi bi-pencil" onclick="changeInfo('email')"></i>`);
-            $("#accountPhoneNumber").html(``+ json.phoneNumber +`<i class="bi bi-pencil" onclick="changeInfo('phoneNumber')"></i>`);
+            $("#accountPhoneNumber").html(``+ json.phoneNumber +`<i class="bi bi-pencil" onclick=""></i>`);
+            $("#notes").html(json.notes);
         });
-        // this.nameRow.innerHTML = '<p>'+ name +'<i class="bi bi-pencil" onclick="changeName()"></i></p>';
-        // $(".")
     }
     else if (this.id == "product")
     {
@@ -58,20 +57,123 @@ async function changeInfo(sort) {
             <i class="bi bi-x-circle" onclick="denyName()"></i>`
             )
     }
+    else if(sort == "notes")
+    {
+        $("#titleNotes").html(
+            `<b>Notities 
+            <i class="bi bi-check-circle" onclick="acceptName('notes')"></i>
+            <i class="bi bi-x-circle" onclick="denyName()"></i></b>`
+        );
+        $("#notes").html(
+            `<textarea id = "changeNotes" type = "text">`+ json.notes +`</textarea>`
+        );
+    }
 }
 
-function acceptName(type) {
+async function acceptName(type) {
     if(type == "name"){
         let updatedName = $("#nameChange").val();
         console.log(updatedName);
+        let json = {"fullName": updatedName}
+        
+        let res = await ApiCaller.updateUser(json, user.userId)
+        console.log(user.userId);
+        if(res.status == 200){
+            console.log("succes");
+            toastPopUp("Uw naam is geüpdate", "success");
+        }else{
+            console.log("pech");
+        }
+            
+        const userConst = await ApiCaller.getUserInfo(user.userId);
+        const jsonload = await userConst.json();
+        console.log(jsonload);
+        $(".settingsContent").empty();
+        $(".settingsContent").load("profile/myProfile", function (){
+            $("#accountName").html(``+ jsonload.fullName +`<i class="bi bi-pencil" onclick="changeInfo('name')"></i>`);
+            $("#accountEmail").html(``+ jsonload.email +`<i class="bi bi-pencil" onclick="changeInfo('email')"></i>`);
+            $("#accountPhoneNumber").html(``+ jsonload.phoneNumber +`<i class="bi bi-pencil" onclick="changeInfo('phoneNumber')"></i>`);
+            $("#titleNotes").html(`<b>Notities <i class="bi bi-pencil" onclick="changeInfo('notes')"></i></b>`);
+            $("#notes").html(json.notes);
+        })
     }
     else if(type == "email"){
         let updatedEmail = $("#changeEmail").val();
         console.log(updatedEmail);
+        
+        let json = {"email": updatedEmail}
+        
+        let res = await ApiCaller.updateUser(json, user.userId)
+        console.log(user.userId);
+        if(res.status == 200){
+            console.log("succes");
+            toastPopUp("Uw email is geüpdate", "success");
+        }else{
+            console.log("pech");
+        }    
+        const userConst = await ApiCaller.getUserInfo(user.userId);
+        const jsonload = await userConst.json();
+        console.log(jsonload);
+        $(".settingsContent").empty();
+        $(".settingsContent").load("profile/myProfile", function (){
+            $("#accountName").html(``+ jsonload.fullName +`<i class="bi bi-pencil" onclick="changeInfo('name')"></i>`);
+            $("#accountEmail").html(``+ jsonload.email +`<i class="bi bi-pencil" onclick="changeInfo('email')"></i>`);
+            $("#accountPhoneNumber").html(``+ jsonload.phoneNumber +`<i class="bi bi-pencil" onclick="changeInfo('phoneNumber')"></i>`);
+            $("#titleNotes").html(`<b>Notities <i class="bi bi-pencil" onclick="changeInfo('notes')"></i></b>`);
+            $("#notes").html(json.notes);
+        })
     }
     else if(type == "phone"){
         let updatedPhone = $("#changePhoneNumber").val();
         console.log(updatedPhone);
+        
+        let json = {"phoneNumber": updatedPhone}
+        
+        let res = await ApiCaller.updateUser(json, user.userId)
+        console.log(user.userId);
+        if(res.status == 200){
+            console.log("succes");
+            toastPopUp("Uw telefoonnummer is geüpdate", "success");
+        }else{
+            console.log("pech");
+        }    
+        const userConst = await ApiCaller.getUserInfo(user.userId);
+        const jsonload = await userConst.json();
+        console.log(jsonload);
+        $(".settingsContent").empty();
+        $(".settingsContent").load("profile/myProfile", function (){
+            $("#accountName").html(``+ jsonload.fullName +`<i class="bi bi-pencil" onclick="changeInfo('name')"></i>`);
+            $("#accountEmail").html(``+ jsonload.email +`<i class="bi bi-pencil" onclick="changeInfo('email')"></i>`);
+            $("#accountPhoneNumber").html(``+ jsonload.phoneNumber +`<i class="bi bi-pencil" onclick="changeInfo('phoneNumber')"></i>`);
+            $("#titleNotes").html(`<b>Notities <i class="bi bi-pencil" onclick="changeInfo('notes')"></i></b>`);
+            $("#notes").html(json.notes);
+        })
+    }
+    else if(type == "notes"){
+        let updatedNotes = $("#changeNotes").val();
+        console.log(updatedNotes);
+        
+        let json = {"notes": updatedNotes}
+        
+        let res = await ApiCaller.updateUser(json, user.userId)
+        console.log(user.userId);
+        if(res.status == 200){
+            console.log("succes");
+            toastPopUp("Uw notities zijn geüpdate", "success");
+        }else{
+            console.log("pech");
+        }    
+        const userConst = await ApiCaller.getUserInfo(user.userId);
+        const jsonload = await userConst.json();
+        console.log(jsonload);
+        $(".settingsContent").empty();
+        $(".settingsContent").load("profile/myProfile", function (){
+            $("#accountName").html(``+ jsonload.fullName +`<i class="bi bi-pencil" onclick="changeInfo('name')"></i>`);
+            $("#accountEmail").html(``+ jsonload.email +`<i class="bi bi-pencil" onclick="changeInfo('email')"></i>`);
+            $("#accountPhoneNumber").html(``+ jsonload.phoneNumber +`<i class="bi bi-pencil" onclick="changeInfo('phoneNumber')"></i>`);
+            $("#titleNotes").html(`<b>Notities <i class="bi bi-pencil" onclick="changeInfo('notes')"></i></b>`);
+            $("#notes").html(json.notes);
+        })
     }
 }
 
