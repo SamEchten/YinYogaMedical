1808214ab0de57f8b1790d2a21244e55b4d882f0
SamEchten <SamEchten@users.noreply.github.com>
2022-06-13 13:24:22 +0200

    Dashboard js files

--- config.js
+++ config.js
@@ -19,5 +19,5 @@ module.exports.config = {
         },
         url: "https://het-eigen-wijze-lichaam.nl"
     },
-    webhookUrl: "https://1032-194-171-182-60.eu.ngrok.io"
+    webhookUrl: "https://c913-194-171-182-60.eu.ngrok.io"
 }
